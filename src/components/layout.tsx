45574498023ea9176fc96c19521688fe214e425a
BrianWong05 <wongtiniaomacau@gmail.com>
2025-03-08 12:48:11 +1100

    fix: change page go to top

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { useLocation } from "react-router-dom";
 
 interface ILayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FunctionComponent<ILayoutProps> = ({ children }) => {
+  const pathname = useLocation().pathname;
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div>
       <Header />
