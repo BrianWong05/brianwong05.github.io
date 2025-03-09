f40cc2c2cb7718df5ad7cc865c99272c3f55cc08
BrianWong05 <wongtiniaomacau@gmail.com>
2025-03-09 14:12:07 +1100

    style: remove top margin

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -7,13 +7,13 @@ import * as React from "react";
 const Projects: React.FunctionComponent = () => {
   return (
     <Layout>
-      <div className="mt-20">
+      <div className="mt-28">
         <SectionHeader
           header="A Collection of My Best Work"
           title="Signature Creations"
           description="Check out how I create engaging digital solutions from concepts."
         />
-        <div className="mt-8 md:mx-8 lg:mx-16">
+        <div className="md:mx-8 lg:mx-16">
           <div className="mb-100 grid grid-cols-1 lg:grid-cols-2">
             {projectList.map((project) => {
               return <ProjectCard project={project} />;
