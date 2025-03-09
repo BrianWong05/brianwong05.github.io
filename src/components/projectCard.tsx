417ecce6d6f56494d049a5c33756b0f1f65fffc4
BrianWong05 <wongtiniaomacau@gmail.com>
2025-03-09 13:35:26 +1100

    feat: create project card component

---
+++ src/components/projectCard.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Button } from "./ui/button";
+import { ExternalLinkIcon } from "lucide-react";
+import { project } from "@/types";
+
+interface IProjectCardProps {
+  project: project;
+  isSticky?: boolean;
+  index?: number;
+  gap?: number;
+}
+
+const ProjectCard: React.FunctionComponent<IProjectCardProps> = ({
+  project,
+  isSticky = false,
+  index = 0,
+  gap = 45,
+}) => {
+  return (
+    <div className={`relative p-8 ${isSticky && "sticky"}`} style={{ top: `${index * gap + 50}px` }}>
+      <div className="absolute -z-51 m-8 inset-0 bg-background rounded-2xl" />
+      <Card className="p-8 bg-secondary/20 hover:bg-secondary/40 transition duration-200 curso-poi md:pt-12 md:px-10 lg:h-100 cursor-pointer group ">
+        <a href={project.link} target="_blank">
+          <CardHeader className="px-0">
+            <CardDescription>
+              <div className="gap-2 bg-gradient-to-r from-[#254a7b] to-[#5693d9] dark:bg-gradient-to-r dark:from-[#6496d8] dark:to-[#adc8e8] text-transparent bg-clip-text inline-flex font-bold uppercase tracking-widest text-sm">
+                <span>{project.company}</span>
+                <span>&bull;</span>
+                <span>{project.year}</span>
+              </div>
+            </CardDescription>
+            <CardTitle>
+              <h3 className="text-2xl md:text-4xl mt-2 md:mt-5">{project.title}</h3>
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="px-0 w-full mt-8">
+            <ul className="flex flex-col gap-4 md:mt-5">
+              {project.results.map((result) => {
+                return (
+                  <li key={result.title} className="flex w-full gap-2 text-sm md:text-base text-foreground/70">
+                    {result.title}
+                  </li>
+                );
+              })}
+            </ul>
+            <Button className="h-12 w-full rounded-xl font-semibold inline-flex gap-2 mt-8 md:w-50 cursor-pointer transition-all duration-200">
+              <span>View Live Site</span>
+              <ExternalLinkIcon className="size-4" />
+            </Button>
+            <div className="absolute top-0 left-1/2 -translate-x-1/2 -z-50 opacity-30 w-full h-full overflow-clip p-8">
+              <div className="absolute m-8 inset-0 bg-[#9cc8ef] dark:bg-[#102137] dark:group-hover:bg-[#143055] mix-blend-soft-light rounded-2xl" />
+              <div className="relative w-full h-full ">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="rounded-2xl grayscale-30 group-hover:grayscale-10 h-full w-full object-cover"
+                />
+              </div>
+            </div>
+            <div className="hidden hover:text-ring">
+              <ExternalLinkIcon className="size-6" />
+            </div>
+          </CardContent>
+          {/* <CardFooter className="px-0">
+                      <p>Card Footer</p>
+                    </CardFooter> */}
+        </a>
+      </Card>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { projectList } from "@/assets/projectList";
 import { Button } from "@/components/ui/button";
-import { ExternalLinkIcon, MoveRight } from "lucide-react";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { MoveRight } from "lucide-react";
 import SectionHeader from "@/sections/header";
 import { Link } from "react-router-dom";
+import ProjectCard from "@/components/projectCard";
+import { useTailwindBreakpoint } from "@/components/useTailwindBreakpoint";
 
 const ProjectSection: React.FunctionComponent = () => {
+  const breakpoint = useTailwindBreakpoint();
   return (
     <div className="pb-16 mt-30 lg:py-24 lg:relative">
       <div>
@@ -15,67 +17,13 @@ const ProjectSection: React.FunctionComponent = () => {
           title="Highlighted Work"
           description="Explore how I’ve turned ideas into captivating digital solutions."
         />
-        <div className="flex flex-col mt-10 gap-20 m-8 md:mt-20 md:mx-20 lg:mx-0 lg:w-full xl:max-w-330 xl:mx-auto lg:[&>div:nth-child(odd)>div]:left-30 lg:[&>div:nth-child(even)>div]:right-30 lg:[&>div:nth-child(odd)>div]:text-left lg:[&>div:nth-child(even)>div]:text-right lg:[&>div:nth-child(odd)>a>img]:right-30 lg:[&>div:nth-child(even)>a>img]:left-30 lg:[&>div:nth-child(even)>div>div>div>div>a]:right-8">
-          {projectList.slice(0, 3).map((project) => {
-            return (
-              <div key={project.title} className="relative bg-background lg:flex lg:mb-95">
-                <a href={project.link} target="_blank">
-                  <img
-                    src={project.image}
-                    alt={project.title}
-                    className="hidden absolute rounded-xl drop-shadow-2xl cursor-pointer lg:block max-w-lg xl:max-w-xl top-22 xl:top-10 opacity-70 lg:fill-accent hover:opacity-100 transition-all duration-300"
-                  />
-                </a>
-                <div className="lg:absolute lg:w-xl bg-background rounded-xl lg:mt-25 drop-shadow-2xl">
-                  <Card className="lg:relative p-8 bg-secondary/20 md:pt-12 md:px-10 md:pb-0 lg:pt-0 lg:pb-4">
-                    <CardHeader className="px-0 lg:absolute lg:-top-20 lg:block lg:w-full lg:pr-20">
-                      <CardDescription>
-                        <div className="gap-2 bg-gradient-to-r from-[#254a7b] to-[#5693d9] dark:bg-gradient-to-r dark:from-[#6496d8] dark:to-[#adc8e8] text-transparent bg-clip-text inline-flex font-bold uppercase tracking-widest text-sm">
-                          <span>{project.company}</span>
-                          <span>&bull;</span>
-                          <span>{project.year}</span>
-                        </div>
-                      </CardDescription>
-                      <CardTitle>
-                        <h3 className="text-2xl md:text-4xl mt-2 md:mt-5 lg:mt-2">{project.title}</h3>
-                      </CardTitle>
-                    </CardHeader>
-                    <hr className="border-t-2 mt-4 md:mt-5 lg:hidden" />
-                    <CardContent className="px-0 w-full">
-                      <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                        {project.results.map((result) => {
-                          return (
-                            <li
-                              key={result.title}
-                              className="flex w-full gap-2 text-sm md:text-base text-foreground/70 lg:block"
-                            >
-                              {result.title}
-                            </li>
-                          );
-                        })}
-                      </ul>
-                      <a href={project.link} target="_blank">
-                        <Button className="h-12 w-full rounded-xl font-semibold inline-flex gap-2 mt-8 md:w-50 lg:hidden cursor-pointer transition-all duration-200">
-                          <span>View Live Site</span>
-                          <ExternalLinkIcon className="size-4" />
-                        </Button>
-                      </a>
-                      <div className="mt-8 lg:hidden">
-                        <img src={project.image} alt={project.title} className="rounded-2xl" />
-                      </div>
-                      <div className="hidden lg:flex hover:text-ring">
-                        <a href={project.link} target="_blank" className="absolute -bottom-10">
-                          <ExternalLinkIcon className="size-6" />
-                        </a>
-                      </div>
-                    </CardContent>
-                    {/* <CardFooter className="px-0">
-                      <p>Card Footer</p>
-                    </CardFooter> */}
-                  </Card>
-                </div>
-              </div>
-            );
+        {/* <div className="flex flex-col mt-10 gap-20 m-8 md:mt-20 md:mx-20 lg:mx-0 lg:w-full xl:max-w-330 xl:mx-auto lg:[&>div:nth-child(odd)>div]:left-30 lg:[&>div:nth-child(even)>div]:right-30 lg:[&>div:nth-child(odd)>div]:text-left lg:[&>div:nth-child(even)>div]:text-right lg:[&>div:nth-child(odd)>a>img]:right-30 lg:[&>div:nth-child(even)>a>img]:left-30 lg:[&>div:nth-child(even)>div>div>div>div>a]:right-8"> */}
+        <div className="grid grid-cols-1 mt-10 gap-6 my-8 md:mt-20 md:mx-20 lg:mx-0 lg:w-full xl:max-w-330 xl:mx-auto">
+          {projectList.slice(0, 3).map((project, index) => {
+            let gap = 110;
+            if (breakpoint === "md") gap = 150;
+            else if (breakpoint === "lg") gap = 170;
+            return <ProjectCard key={index} project={project} isSticky={true} index={index} gap={gap} />;
           })}
         </div>
       </div>
